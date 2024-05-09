53864f09dd0bb670832a88e9c1b99a11798429f5
Mert <mertarslan260@gmail.com>
2024-05-09 14:50:22 +0300

    C# Örneklerim
    Örnek 9 10

---
+++ ornek10/ornek10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // {1,3,4,34,41,56,89} dizisindeki sayılardan hangileri 3'ün katıdır
+            int[] sayilar = { 1, 3, 4, 34, 41, 56, 89, 12};
+
+            for (int i = 0; i < sayilar.Length; i++)
+                if (sayilar[i]%3==0)
+                    Console.WriteLine(sayilar[i]);
+                
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ornek9/ornek9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 ile girilen sayı arasındakilerin toplamını hesaplayınız.
+            Console.Write("Sayiyi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = 0;
+            for (int i = 1; i <= sayi; i++)
+            {
+                toplam += i;
+            }
+
+            Console.WriteLine($"Toplam: {toplam}");
+            Console.ReadKey();
+        }
+    }
+}
