49b1e3ac426262f09a18834d4fcea1805ed2cf52
Mert <mertarslan260@gmail.com>
2024-05-10 12:45:08 +0300

    C# Örneklerim
    Örnek 12

---
+++ ornek12/ornek12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = File.OpenText("deneme.txt"); // vs için C:\Users\UserX\source\repos\ornek12\ornek12\bin\Debug default olarak içine atılmalı
+
+            String s = "";
+            while ((s = sr.ReadLine()) != null) {
+                Console.WriteLine(s); 
+            }
+            Console.ReadKey();
+        }
+    }
+}   
