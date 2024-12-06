207a7e6fb7914f828b3d9d41e663530b156541da
Mert <mertarslan260@gmail.com>
2024-12-06 14:34:39 +0300

    C# Çalışma

---
+++ AdoNetDemo/AdoNetDemo/ProductDal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal //DAL : DATA ACCESS OBJECT
+    {
+        public List<Product> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;inital catalog=ETread;integrated security=true");
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open(); // Bağlantı açma işlemi kontrolle yapılır
+            }
+            SqlCommand command = new SqlCommand("Select * from Products",connection);
+
+            SqlDataReader reader = command.ExecuteReader(); // Select komutu için bu metod kullanılır
+
+            // DataTable dt = new DataTable();
+            // dt.Load(reader);
+
+            reader.Close();
+            connection.Close();
+            return new List<Product>();
+        }
+    }
+}
