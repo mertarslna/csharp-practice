d97e67bfcba2ba23fd6ee8d1ab94d16d6bf78ec8
Mert <mertarslan260@gmail.com>
2024-12-14 21:37:31 +0300

    C# Çalışma

---
+++ Attributes/Attributes/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer
+            {
+                Id = 1,
+                LastName = "Arslan",
+                Age = 32
+            };
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+            Console.ReadKey();
+        }
+    }
+
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add, instead use AddNew Method")] 
+        // Hazır Attribute kullanan kişiye eski metod kullandığının uyarısını verir
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!",
+                customer.Id,customer.FirstName,customer.LastName,customer.Age);
+        }
+
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!",
+                customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+
+    class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            _tableName = tableName;
+        }
+    }
+
+}
