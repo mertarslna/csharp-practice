bfa74c38ad8536b97ed28cc8eabae36009536762
Mert <mertarslan260@gmail.com>
2025-07-16 15:56:36 +0300

    C# Examples
    ASCII Trial

---
+++ AlgoritmaTasarimi/ASCII.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmaTasarimi
+{
+    internal class ASCII
+    {
+        public static void Print()
+        {
+            for (int i = 32; i < 126; i++)
+            {
+                Console.Write("{0,5} : {1} ",i,(char)i);
+                if(i % 5 == 0)
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
