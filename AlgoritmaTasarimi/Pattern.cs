6f75a8d21ed201553e0d1beec49c5bdcff7ab33f
Mert <mertarslan260@gmail.com>
2025-07-16 16:33:35 +0300

    C# Examples
    Update for Pattern class

---
+++ AlgoritmaTasarimi/Pattern.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmaTasarimi
+{
+    internal class Pattern
+    {
+        /// <summary>
+        /// Desen oluşturur.
+        /// </summary>
+        /// <param name="c">Karakter tanımı</param>
+        /// <param name="n">Tekrar sayısı</param>
+        public static void Create1(char c = '*', int n=5)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string(c, i));
+            }
+        }
+        // @Override
+        public static void Create1(string c = "*", int n = 5)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < i; j++) 
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+            }
+        }
+        /// <summary>
+        /// Desen oluşturur.
+        /// </summary>
+        /// <param name="c">Karakter tanımı</param>
+        /// <param name="n">Tekrar sayısı</param>
+        public static void Create2(char c = '!', int n = 5)
+        {
+            for (int i = n; i > 0; i--)
+            {
+                Console.WriteLine(new string(c, i));
+            }
+        }
+        // @Override
+        public static void Create2(string c = "!", int n = 5)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i; j >= 0; j--)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
